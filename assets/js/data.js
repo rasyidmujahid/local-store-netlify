c88251ca0122bccf4c6644eadd1074b556bd8013
Abdurrasyid Mujahid <rasyidmujahid@gmail.com>
2020-05-30 16:09:39 +0700

    data source

---
+++ assets/js/data.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+  el: '#store',
+  data: {
+    stores: [{
+        id: 1,
+        tag: 'food',
+        name: 'Nama Toko',
+        description: 'Jualan apa saja tuliskan di sini',
+        cover_image: 'assets/img/course-3.jpg',
+        seller_photo: 'Seller Name',
+        seller_photo: 'assets/img/trainers/trainer-3.jpg'
+      },
+      {
+        id: 1,
+        tag: 'food',
+        name: 'Nama Toko',
+        description: 'Jualan apa saja tuliskan di sini',
+        cover_image: 'assets/img/course-3.jpg',
+        seller_photo: 'Seller Name',
+        seller_photo: 'assets/img/trainers/trainer-3.jpg'
+      },
+      {
+        id: 1,
+        tag: 'food',
+        name: 'Nama Toko',
+        description: 'Jualan apa saja tuliskan di sini',
+        cover_image: 'assets/img/course-3.jpg',
+        seller_photo: 'Seller Name',
+        seller_photo: 'assets/img/trainers/trainer-3.jpg'
+      },
+      {
+        id: 1,
+        tag: 'food',
+        name: 'Nama Toko',
+        description: 'Jualan apa saja tuliskan di sini',
+        cover_image: 'assets/img/course-3.jpg',
+        seller_photo: 'Seller Name',
+        seller_photo: 'assets/img/trainers/trainer-3.jpg'
+      }
+    ]
+  }
+});
