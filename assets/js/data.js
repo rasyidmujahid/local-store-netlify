52bc9e1e8f86b9cd7dca0ef6010f4841ad41db2f
Abdurrasyid Mujahid <rasyidmujahid@gmail.com>
2020-05-30 21:02:06 +0700

    update store data

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,39 +3,44 @@ var app = new Vue({
   data: {
     stores: [{
         id: 1,
-        tag: 'food',
-        name: 'Nama Toko',
-        description: 'Jualan apa saja tuliskan di sini',
-        cover_image: 'assets/img/course-3.jpg',
-        seller_photo: 'Seller Name',
-        seller_photo: 'assets/img/trainers/trainer-3.jpg'
+        tag: ['sembako', 'makanan'],
+        name: 'Toko Raffa',
+        description: 'Sembako dan aneka kue',
+        cover_image: 'assets/img/sembako.jpg',
+        seller_name: 'Mega',
+        mobile_no: '082110873440',
+        whatsapp: 'https://wa.me/62821108734401',
+        instagram: 'https://www.instagram.com/r3_kitchen'
+      },
+      {
+        id: 2,
+        tag: ['sembako'],
+        name: 'Toko Eza Mitra Usaha',
+        description: 'Sembako dan pampers',
+        cover_image: 'assets/img/sembako.jpg',
+        seller_name: 'Ajo',
+        mobile_no: '087885727559',
+        whatsapp: 'https://wa.me/6287885727559'
+      },
+      {
+        id: 3,
+        tag: ['sembako'],
+        name: 'Toko Kosama',
+        description: 'Air mineral dan sembako',
+        cover_image: 'assets/img/galon.jpg',
+        seller_name: 'Saleh',
+        mobile_no: '081210971405',
+        whatsapp: 'https://wa.me/6281210971405'
       },
       {
         id: 1,
-        tag: 'food',
-        name: 'Nama Toko',
-        description: 'Jualan apa saja tuliskan di sini',
-        cover_image: 'assets/img/course-3.jpg',
-        seller_photo: 'Seller Name',
-        seller_photo: 'assets/img/trainers/trainer-3.jpg'
-      },
-      {
-        id: 1,
-        tag: 'food',
-        name: 'Nama Toko',
-        description: 'Jualan apa saja tuliskan di sini',
-        cover_image: 'assets/img/course-3.jpg',
-        seller_photo: 'Seller Name',
-        seller_photo: 'assets/img/trainers/trainer-3.jpg'
-      },
-      {
-        id: 1,
-        tag: 'food',
-        name: 'Nama Toko',
-        description: 'Jualan apa saja tuliskan di sini',
-        cover_image: 'assets/img/course-3.jpg',
-        seller_photo: 'Seller Name',
-        seller_photo: 'assets/img/trainers/trainer-3.jpg'
+        tag: ['kesehatan'],
+        name: 'Toko Galeri Madina (E3.18)',
+        description: 'Madu dan herbal',
+        cover_image: 'assets/img/madu.jpg',
+        seller_name: 'Arif',
+        mobile_no: '085624969749',
+        whatsapp: 'https://wa.me/6285624969749'
       }
     ]
   }
