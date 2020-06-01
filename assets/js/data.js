1de8a7b2631fc32155b178c16fb16b381b2b058e
Abdurrasyid Mujahid <rasyidmujahid@gmail.com>
2020-06-01 16:14:07 +0700

    some more data

--- assets/js/data.js
+++ assets/js/data.js
@@ -6,7 +6,7 @@ var app = new Vue({
         tag: ['sembako', 'makanan'],
         name: 'Toko Raffa',
         description: 'Sembako dan aneka kue',
-        cover_image: 'assets/img/sembako.jpg',
+        cover_image: 'assets/img/aneka-kue.jpg',
         seller_name: 'Mega',
         mobile_no: '082110873440',
         whatsapp: 'https://wa.me/62821108734401',
@@ -20,9 +20,7 @@ var app = new Vue({
         cover_image: 'assets/img/sembako.jpg',
         seller_name: 'Ajo',
         mobile_no: '087885727559',
-        whatsapp: 'https://wa.me/6287885727559',
-        instagram: 'https://wa.me/6287885727559',
-        // facebook: 'https://wa.me/6287885727559'
+        whatsapp: 'https://wa.me/6287885727559'
       },
       {
         id: 3,
@@ -35,7 +33,7 @@ var app = new Vue({
         whatsapp: 'https://wa.me/6281210971405'
       },
       {
-        id: 5,
+        id: 4,
         tag: ['kesehatan'],
         name: 'Toko Galeri Madina (E3.18)',
         description: 'Madu dan herbal',
@@ -46,53 +44,63 @@ var app = new Vue({
       },
       {
         id: 5,
-        tag: ['food'],
+        tag: ['makanan'],
         name: 'Ozora Dimsum Kuliner',
         description: '',
         cover_image: 'assets/img/dimsum.jpg',
         seller_name: 'Franky',
         mobile_no: '085214929500',
         whatsapp: 'https://wa.me/6285214929500'
-      }, 
+      },
       {
-        id: 5,
-        tag: ['food'],
-        name: 'Ozora Dimsum Kuliner',
-        description: '',
-        cover_image: 'assets/img/dimsum.jpg',
-        seller_name: 'Franky',
-        mobile_no: '085214929500',
-        whatsapp: 'https://wa.me/6285214929500'
-      }, 
+        id: 6,
+        tag: ['makanan'],
+        name: 'Ezra Celullar',
+        description: 'Ayam Bahar (Kuliner Ayam Bakar)',
+        cover_image: 'assets/img/ayam-bakar.jpg',
+        seller_name: 'Bahar',
+        mobile_no: '081295450548/085591401909',
+        whatsapp: 'https://wa.me/6281295450548'
+      },
       {
-        id: 5,
-        tag: ['food'],
-        name: 'Ozora Dimsum Kuliner',
-        description: '',
-        cover_image: 'assets/img/dimsum.jpg',
-        seller_name: 'Franky',
-        mobile_no: '085214929500',
-        whatsapp: 'https://wa.me/6285214929500'
-      }, 
+        id: 7,
+        tag: ['makanan'],
+        name: 'Toko Juli',
+        description: 'Kuliner Mie ayam & sambal bawang, Aqua & Telur Ayam',
+        cover_image: 'assets/img/mie-ayam.jpg',
+        seller_name: 'Ibu Juli',
+        mobile_no: '081287308995',
+        whatsapp: 'https://wa.me/6281287308995'
+      },
       {
-        id: 5,
-        tag: ['food'],
-        name: 'Ozora Dimsum Kuliner',
-        description: '',
-        cover_image: 'assets/img/dimsum.jpg',
-        seller_name: 'Franky',
-        mobile_no: '085214929500',
-        whatsapp: 'https://wa.me/6285214929500'
-      }, 
+        id: 8,
+        tag: ['makanan'],
+        name: 'Pia, bakso,ikan dori,kentang Frozen,kue dan kue kering',
+        description: 'Pia, bakso,ikan dori,kentang Frozen,kue dan kue kering',
+        cover_image: 'assets/img/pia.jpg',
+        seller_name: '[]',
+        mobile_no: '081315779438',
+        whatsapp: 'https://wa.me/6281315779438'
+      },
       {
-        id: 5,
-        tag: ['food'],
-        name: 'Ozora Dimsum Kuliner',
-        description: '',
-        cover_image: 'assets/img/dimsum.jpg',
-        seller_name: 'Franky',
-        mobile_no: '085214929500',
-        whatsapp: 'https://wa.me/6285214929500'
+        id: 9,
+        tag: ['makanan', 'kesehatan'],
+        name: 'Kios SK',
+        description: 'Sambal Kacang Pecel Madiun, Kebab & Dettol cair Antiseptik',
+        cover_image: 'assets/img/pecel.jpg',
+        seller_name: 'Ibu Daniel',
+        mobile_no: '085776452036',
+        whatsapp: 'https://wa.me/6285776452036'
+      },
+      {
+        id: 10,
+        tag: ['makanan'],
+        name: 'Murni / Mustofa',
+        description: 'jual aneka menu takjilan, kripik pisang dan Telur ayam',
+        cover_image: 'assets/img/keripik-pisang.jpg',
+        seller_name: 'Murni / Mustofa',
+        mobile_no: '081382107077, 081398650908',
+        whatsapp: 'https://wa.me/6281382107077'
       }
     ]
   }
